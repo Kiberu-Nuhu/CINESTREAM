ff5336be47a2b7b6c0fe091d6e66078dc597c8c8
Kiberu Nuhu <kiberunuhu547@gmail.com>
2023-09-26 17:54:14 +0300

    Adding unit tests

---
+++ src/components/Search/Section3.test.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { render } from '@testing-library/react';
+import Section3 from './Section3';
+import { fireEvent } from "@testing-library/react";
+import { waitFor } from "@testing-library/react";
+
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+const mockAxios = new MockAdapter(axios);
+
+test('fetchMovieData function is called when the "Search" button is clicked', async () => {
+    mockAxios.onGet(/omdbapi/).reply(200, { Search: [] });
+    const { getByText, getByPlaceholderText } = render(<Section3 />);
+    const input = getByPlaceholderText('Type a movie name');
+    const searchButton = getByText('Search');
+    fireEvent.change(input, { target: { value: 'Avengers' } });
+    fireEvent.click(searchButton);
+  
+    // Wait for the fetchMovieData function to complete
+    await waitFor(() => {
+      expect(mockAxios.history.get.length).toBe(1); // Ensure Axios was called
+      // Add more assertions as needed
+    });
+  });
+  
